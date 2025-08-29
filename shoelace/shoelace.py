b05630283cdb956f8b0fe970650694bf79f9cae0
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-29 17:52:02 +0530

    basic foundation created

--- shoelace/shoelace.py
+++ shoelace/shoelace.py
@@ -9,8 +9,14 @@ def shoelace_formula(boundary_points, absoluteValue=True):
         raise SystemExit("Lengths of x and y must be equal")
 
     sum_one = 0
+    sum_two = 0
     for i in range(len_x-1):
         sum_one = sum_one + (x[i] * y[i+1])
+        sum_two = sum_two + (y[i]*x[i+1])
 
-    
+    if absoluteValue:
+        return abs(sum_one-sum_two)
+
+    return (sum_one-sum_two)
+
 
