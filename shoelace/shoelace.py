ae424cf2db17895a8d38f193d4326da46241a61d
somelamedude-git <cozydeveloper843@gmail.com>
2025-08-29 17:57:16 +0530

    code converted to numpy, i had to wrap my head around it

--- shoelace/shoelace.py
+++ shoelace/shoelace.py
@@ -1,22 +1,10 @@
 import numpy as np
 
 def shoelace_formula(boundary_points, absoluteValue=True):
-    x, y = zip(*boundary_points)
-    len_x = len(x)
-    len_y = len(y)
+    points = np.array(boundary_points)
+    x = points[:, 0]
+    y = points[:, 1]
 
-    if len_x != len_y:
-        raise SystemExit("Lengths of x and y must be equal")
+    area = 0.5 * np.sum(x * np.roll(y, -1) - y * np.roll(x, -1))
 
-    sum_one = 0
-    sum_two = 0
-    for i in range(len_x-1):
-        sum_one = sum_one + (x[i] * y[i+1])
-        sum_two = sum_two + (y[i]*x[i+1])
-
-    if absoluteValue:
-        return abs(sum_one-sum_two)
-
-    return (sum_one-sum_two)
-
-
+    return abs(area) if absoluteValue else area
