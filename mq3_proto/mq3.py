5082a8befc43b29e4b3bfb82b0fa1d0cc5ec6a6a
somelamedude-git <cozydeveloper843@gmail.com>
2025-09-04 15:08:32 +0530

    arduino code done

--- mq3_proto/mq3.py
+++ mq3_proto/mq3.py
@@ -1,4 +1,6 @@
 import serial
+import RPi.GPIO as GPIO
+import time
 
 ser = serial.Serial(
         port ='',
@@ -8,3 +10,19 @@ ser = serial.Serial(
         bytesize= serial.EIGHTBITS,
         timeout = 1
         )
+
+digital_output_pin = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(digital_output_pin, GPIO.IN)
+try:
+    while True:
+        sensor_state = GPIO.input(digital_output_pin)
+        if sensor_state == GPIO.HIGH:
+            print('Awesome')
+        else:
+            print('Not so awesome')
+        time.sleep(1)
+except KeyboardInterrupt:
+    print('Exiting')
+finally:
+    GPIO.cleanup()
