f10b1b0eb5770ad04544d7eb85a769c3f10ce9be
somelamedude-git <cozydeveloper843@gmail.com>
2025-09-04 16:02:11 +0530

    setup the code for mean values

--- mq3_proto/analog_mq3.py
+++ mq3_proto/analog_mq3.py
@@ -5,12 +5,20 @@ try:
     ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 except serial.SerialException:
     print("Arduino not connected")
-
+answer = 0
+count = 0
 while True:
     if ser.in_waiting>0:
         line = ser.readline().decode('utf-8').strip()
+        count = count + 1
         try:
             sensor_value = int(line)
-            print(sensor_value)
+            answer= answer+sensor_value
+            if count == 100:
+                answer = answer/float(count)
+                print(answer)
+                count = 0
+                answer = 0
         except ValueError:
             print('Value error')
+    time.sleep(1)
